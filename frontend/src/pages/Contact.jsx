8b7c2e6e47e4607aba47f9115d09c110856c8ea8
Ritik Tyagi <ritiktyagi287@gmail.com>
2026-02-16 13:26:11 +0530

    Project completed

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,38 +1,114 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
   return (
     <>
-      <h1 className='flex justify-center py-10 text-2xl font-bold'>Contact Us </h1>
-      <div className='flex flex-col md:flex-row px-25'>
-        {/* left side  */}
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p className="">
+          Contact <span className="text-gray-700 font-semibold">Us</span>
+        </p>
+        <div className=" my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+          {/* left side  */}
 
-        <img className="w-full sm:w-3/4 md:w-1/2 max-w-5xl md:max-w-6xl lg:max-w-7xl border border-gray-400 rounded-xl shadow-xl bg-white " src={assets.contact_image} alt="" />
+          <img
+            className="w-full md:max-w-90 shadow-lg rounded-lg border border-gray-200 "
+            src={assets.contact_image}
+            alt=""
+          />
 
+          <div className="flex flex-col justisfy-center items-start gap-6  text-justify">
+            {/* right side  */}
+            <p className="text-lg font-semibold text-gray-600">Our Office </p>
 
+            <p className="text-gray-500">
+              54709 Willms Station
+              <br />
+              Suite 350, Washington, USA
+              <br />
+              <p className="text-gray-600">Tel: (415) 555‑0132</p>
+             
+             <span className="text-gray-600">Email:</span>  ritiktyagi287@gmail.com
+            </p>
 
-        <div className='text-gray-500 px-25 mt-10 md:py-0 '>
-          {/* right side  */}
-          <h1 className='text-2xl font-semibold '>Our Office </h1><br />
-          <p>54709 Willms Station<br />
-            Suite 350, Washington, USA<br /><br />
-            Tel: (415) 555‑0132<br />
-            Email: ritiktyagi287@gmail.com</p>
-
-          <div className='text-gray-500'>
-            <h1 className='text-2xl font-semibold py-5'>Careers AT PRESCRIPTO</h1>
-            <p className='text-sm text-justify'>Learn more about our teams and job openings.</p><br />
-            <button className='border flex justify-center items-center w-1/2 h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200'>Explore Jobs</button>
+            <div className="text-gray-500">
+              <h1 className="text-lg text-gray-600 pb-5">
+                Careers At{' '}
+                <span className="text-2xl font-semibold pb-5">PRESCRIPTO</span>
+              </h1>
+              <p className="text-sm text-justify">
+                Learn more about our teams and job openings.
+              </p>
+              <br />
+              <button className="border flex justify-center items-center w-1/2 h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200">
+                Explore Jobs
+              </button>
+            </div>
           </div>
         </div>
-
-
-
       </div>
 
+       <div>
+                  <footer className="relative overflow-hidden px-6 md:px-12 lg:px-20 xl:px-25 w-full text-sm text-slate-500 bg-white pt-10 mt-10">
+                      <svg className="hidden md:block absolute -bottom-30 -left-80 opacity-5 w-full h-full pointer-events-none" width="68"
+                          height="26" viewBox="0 0 68 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <g clip-path="url(#clip0_8678_1074)">
+                              <path d="M16.141 0C13.4854 0 10.9387 1.04871 9.06091 2.91543L2.93268 9.00761C1.05492 10.8743 0 13.4061 0 16.0461C0 21.5435 4.48289 26 10.0128 26C12.6684 26 15.2152 24.9512 17.0929 23.0845L21.3319 18.8705C21.3319 18.8705 21.3319 18.8706 21.3319 18.8705L33.6827 6.59239C34.5795 5.70086 35.7958 5.2 37.0641 5.2C39.1874 5.2 40.9876 6.57576 41.6117 8.47953L45.5096 4.60457C43.7314 1.83589 40.6134 0 37.0641 0C34.4085 0 31.8617 1.04871 29.984 2.91543L13.3943 19.4076C12.4974 20.2992 11.2811 20.8 10.0128 20.8C7.37176 20.8 5.23077 18.6716 5.23077 16.0461C5.23077 14.7852 5.73459 13.5761 6.63139 12.6845L12.7596 6.59239C13.6564 5.70086 14.8727 5.2 16.141 5.2C18.2645 5.2 20.0645 6.57582 20.6887 8.47965L24.5866 4.60466C22.8084 1.83595 19.6904 0 16.141 0Z" fill="#364153" />
+                              <path d="M34.3188 19.4076C33.422 20.2992 32.2056 20.8 30.9373 20.8C28.8143 20.8 27.0143 19.4246 26.39 17.5211L22.4922 21.396C24.2705 24.1643 27.3883 26 30.9373 26C33.5929 26 36.1397 24.9512 38.0175 23.0845L54.6072 6.59239C55.504 5.70086 56.7203 5.2 57.9886 5.2C60.6297 5.2 62.7707 7.32839 62.7707 9.95393C62.7707 11.2148 62.2669 12.4239 61.37 13.3155L55.2419 19.4076C54.345 20.2992 53.1287 20.8 51.8604 20.8C49.7372 20.8 47.9371 19.4243 47.3129 17.5207L43.4151 21.3957C45.1933 24.1642 48.3112 26 51.8604 26C54.516 26 57.0628 24.9512 58.9405 23.0845L65.0687 16.9924C66.9465 15.1257 68.0014 12.5939 68.0014 9.95393C68.0014 4.45652 63.5186 0 57.9886 0C55.333 0 52.7863 1.04871 50.9085 2.91543L34.3188 19.4076Z" fill="#364153" />
+                          </g>
+                          <defs>
+                              <clipPath id="clip0_8678_1074">
+                                  <rect width="68" height="26" fill="white" />
+                              </clipPath>
+                          </defs>
+                      </svg>
+                      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
+                          <div className="sm:col-span-2 lg:col-span-1">
+                              <img className="h-auto w-auto rounded-full" src={assets.logo} alt="" />
+                              <p className="text-sm/7 mt-6 text-justify">
+                                  Our platform makes it easy to find trusted doctors, book appointments online,
+                                  and manage your healthcare needs in one place. With verified specialists,
+                                  flexible scheduling, and secure access to medical services, we help you save
+                                  time and focus on what matters most — your health.
+                              </p>
+      
+                          </div>
+                          <div className="flex flex-col lg:items-center lg:justify-center">
+                              <div className="flex flex-col text-sm space-y-2.5">
+                                  <h2 className="font-semibold mb-5 text-gray-800">Company</h2>
+                                  <a onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="hover:text-slate-600 transition" href="/about">About us</a>
+                                  <a onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="hover:text-slate-600 transition" href="#">Careers</a>
+                                  <a onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="hover:text-slate-600 transition" href="/contact">Contact us</a>
+                                  <a onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="hover:text-slate-600 transition" href="#">Privacy policy</a>
+                              </div>
+                          </div>
+                          <div>
+                              <h2 className="font-semibold text-gray-800 mb-5">Subscribe to our newsletter</h2>
+                              <div className="text-sm space-y-6 max-w-sm">
+                                  <p>The latest news, articles, and resources, sent to your inbox weekly.</p>
+                                  <div className="flex items-center">
+                                      <input className="rounded-l-md bg-gray-100 outline-none w-full max-w-64 h-11 px-3" type="email" placeholder="Enter your email" />
+                                      <button className="bg-linear-to-b from-indigo-600 to-indigo-800 cursor-pointer hover:from-indigo-700 hover:to-indigo-900 transition px-4 h-11 text-white rounded-r-md">
+                                          Subscribe
+                                      </button>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="flex flex-col md:flex-row items-center justify-between gap-4 py-4 border-t mt-6 border-slate-200">
+                          <p className="text-center">
+                              Copyright 2026 © <a href="https://prebuiltui.com">PrebuiltUI</a> All Right Reserved.
+                          </p>
+                          <div className="flex items-center gap-4">
+                              <a href="/">Privacy Policy</a>
+                              <a href="/">Terms of Service</a>
+                              <a href="/">Cookie Policy</a>
+                          </div>
+                      </div>
+                  </footer>
+              </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,142 +1,150 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { AppContext } from '../context/AppContext'
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from "react";
+import { AppContext } from "../context/AppContext";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Doctors = () => {
   const { doctors } = useContext(AppContext);
   const { speciality } = useParams();
-  const [filterDoc, setFilterDoc] = useState([])
-  const Navigate = useNavigate();
-  const slugify = (text) =>
-    text.replace(/\s+/g, '-');
+  const navigate = useNavigate();
 
+  const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter, setShowFilter] = useState(false);
+
+  // slug function
+  const slugify = (text) =>
+    text.toLowerCase().replace(/\s+/g, "-");
+
+  // Handle navigation
   const handleNavigate = (name) => {
-    Navigate(
-      speciality === name
-        ? "/doctors"
-        : `/doctors/${slugify(name)}`
-    );
+    const slug = slugify(name);
+    navigate(speciality === slug ? "/doctors" : `/doctors/${slug}`);
+    setShowFilter(false);
   };
 
-
-  const applyfilter = () => {
+  // Apply filter
+  useEffect(() => {
     if (speciality) {
       setFilterDoc(
         doctors.filter(
-          doc => slugify(doc.speciality) === speciality
+          (doc) => slugify(doc.speciality) === speciality
         )
       );
     } else {
       setFilterDoc(doctors);
     }
-  };
-
-  useEffect(() => {
-    applyfilter();
-  }, [doctors, speciality])
+  }, [doctors, speciality]);
 
   return (
     <>
-      <p className='text-3xl font-medium flex justify-center py-5'>
+      <p className="text-3xl font-medium text-center py-5">
         Browse through the doctors specialist
       </p>
-      <div className='flex flex-col md:flex-row items-start mt-5 gap-5 '>
-        {/* left side */}
-        <div className='text-gray-500'>
-          <div className=" cursor-pointer flex  sm:flex-row md:flex-col md:items-start  gap-6 ">
-            <button
-              onClick={() => handleNavigate("General-physician")}
-              className={`w-full border flex justify-center items-center  h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200 mb-3
-      ${slugify("General physician") === speciality
-                  ? "bg-primary text-white border-primary "
-                  : " border-gray-300 "}
-    `}
-            >
-              General physician
-            </button>
 
-            <button
-              onClick={() => handleNavigate("Gynecologist")}
-              className={`w-full border flex justify-center items-center  h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200 mb-3
-      ${slugify("Gynecologist") === speciality
-                  ? "bg-primary text-white border-primary "
-                  : " border-gray-300 "}
-    `}
-            >
-              Gynecologist
-            </button>
+      <div className="flex flex-col md:flex-row gap-6 relative">
 
-            <button
-              onClick={() => handleNavigate("Dermatologist")}
-              className={`border flex justify-center items-center w-full h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200 mb-3
-      ${slugify("Dermatologist") === speciality
-                  ? "bg-primary text-white border-primary"
-                  : " border-gray-300 "}
-    `}
-            >
-              Dermatologist
-            </button>
-
-            <button
-              onClick={() => handleNavigate("Pediatricians")}
-              className={`border flex justify-center items-center w-full h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200 mb-3
-      ${slugify("Pediatricians") === speciality
-                  ? "bg-primary text-white border-primary"
-                  : " border-gray-300 "}
-    `}
-            >
-              Pediatricians
-            </button>
-
-            <button
-              onClick={() => handleNavigate("Neurologist")}
-              className={`border flex justify-center items-center w-full h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200 mb-3
-      ${slugify("Neurologist") === speciality
-                  ? "bg-primary text-white border-primary"
-                  : " border-gray-300 "}
-    `}
-            >
-              Neurologist
-            </button>
-
-            <button
-              onClick={() => handleNavigate("Gastroenterologist")}
-              className={`border flex justify-center items-center w-full h-10 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md cursor-pointer text-gray-600 font-semibold transition-all duration-200 mb-3 px-5
-      ${slugify("Gastroenterologist") === speciality
-                  ? "bg-primary text-white border-primary"
-                  : " border-gray-300 "}
-    `}
-            >
-              Gastroenterologist
-            </button>
-          </div>
-
-
-
+        {/* Mobile Filter Button */}
+        <div className="md:hidden flex justify-end px-4">
+          <button
+            onClick={() => setShowFilter(true)}
+            className="px-4 py-2 bg-primary text-white rounded-md"
+          >
+            Filter
+          </button>
         </div>
-        {/* Right side  */}
-        <div className="w-full grid grid-cols-4 gap-4 gap-y-6">
-          {
-            filterDoc.map((item, index) => (
-              <div onClick={() => Navigate(`/appointment/${slugify(item.name)}/${item._id}`)} className='border border-blue-200 cursor-pointer overflow-hidden hover:-translate-y-2.5 transiton-all duration-200' key={index} >
-                <img className='bg-[#EAEFFF] ' src={item.image} alt="" />
-                <div className='p-4'>
-                  <div className='flex items-center gap-2 text-center text-sm text-green-500 '>
-                    <p className='h-2 w-2 bg-green-500 rounded-full '></p>   <p className=''> Avaiable</p>
-                  </div>
-                  <p className='text-lg font-semibold'>{item.name}</p>
-                  <p className='text-gray-500'>{item.speciality}</p>
-                </div>
-              </div>
-            ))
-          }
 
+        {/* Overlay (Mobile) */}
+        {showFilter && (
+          <div
+            className="fixed inset-0 bg-black/40 z-40 md:hidden"
+            onClick={() => setShowFilter(false)}
+          ></div>
+        )}
+
+        {/* Sidebar */}
+        <div
+          className={`
+            ${showFilter ? "translate-x-0" : "-translate-x-full"}
+            md:translate-x-0
+            fixed md:relative
+            top-0 left-0
+            h-full md:h-auto
+            w-64 md:w-auto
+            bg-white md:bg-transparent
+            z-50
+            p-6 md:p-0
+            transition-transform duration-300
+          `}
+        >
+          <div className="flex flex-col gap-4 text-gray-600 font-medium">
+
+            {[
+              "General physician",
+              "Gynecologist",
+              "Dermatologist",
+              "Pediatricians",
+              "Neurologist",
+              "Gastroenterologist",
+            ].map((item) => (
+              <button
+                key={item}
+                onClick={() => handleNavigate(item)}
+                className={`border h-10 rounded-md transition-all duration-200
+                  ${
+                    slugify(item) === speciality
+                      ? "bg-primary text-white border-primary"
+                      : "border-gray-300"
+                  }
+                `}
+              >
+                {item}
+              </button>
+            ))}
+
+          </div>
+        </div>
+
+        {/* Doctors Grid */}
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4 md:px-0">
+
+          {filterDoc.map((item) => (
+            <div
+              key={item._id}
+              onClick={() =>
+                navigate(`/appointment/${slugify(item.name)}/${item._id}`)
+              }
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-300 bg-white"
+            >
+              {/* Image Section */}
+              <div className="bg-[#EAEFFF] flex justify-center items-center">
+                <img
+                  className="w-full h-64 object-contain"
+                  src={item.image}
+                  alt={item.name}
+                />
+              </div>
+
+              {/* Info Section */}
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-green-500">
+                  <span className="h-2 w-2 bg-green-500 rounded-full"></span>
+                  <span>Available</span>
+                </div>
+
+                <p className="text-lg font-semibold mt-1">
+                  {item.name}
+                </p>
+
+                <p className="text-gray-500 text-sm">
+                  {item.speciality}
+                </p>
+              </div>
+            </div>
+          ))}
 
         </div>
       </div>
     </>
-
-  )
-}
+  );
+};
 
 export default Doctors;
